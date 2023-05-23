d49fac36743e7ed84a321d1d6974db2f7c57179b
Kostadin Madjerski <kostadin@alphabite.io>
2023-05-23 15:20:24 +0300

    CM-7: Fixed not building

--- src/data/content/index.ts
+++ src/data/content/index.ts
@@ -1,9 +1,8 @@
-export * from './blocksSectionProps';
-export * from './diagramSectionProps';
-export * from './heroSectionProps';
-export * from './planSectionProps';
+export * from './BlocksSectionProps';
+export * from './DiagramSectionProps';
+export * from './HeroSectionProps';
+export * from './PlanSectionProps';
 export * from './startSectionProps';
 export * from './headerProps';
 export * from './footerProps';
 export * from './joinFormProps';
-//neshto
